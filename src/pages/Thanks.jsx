2bdcd1a8a2e7f8a5c9aad7759fbdf825e4e62b1b
FedeSCode <118278708+FedeSCode@users.noreply.github.com>
2024-03-28 23:49:05 +0100

    creation pages

---
+++ src/pages/Thanks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./sideBar.css";
+
+const Thanks = () => {
+  return (
+    <div className="Thanks">
+      <h1>Thanks</h1>
+    </div>
+  );
+};
+
+export default Thanks;
